e1ed4063ea565ebd0c6eda73e6d66c24f67cbe70
kminsoo0110 <kminsoo0110@naver.com>
2025-09-25 14:34:01 +0900

    Add Header Page.

--- Rolling/src/App.jsx
+++ Rolling/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Header from "./components/common/Header";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 // import './App.css'
@@ -8,6 +9,7 @@ function App() {
 
   return (
     <div className="font-bold text-28 text-green-500">
+      <Header />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ Rolling/src/components/common/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+  return (
+    <div className="bg-white text-black ">
+      <div className="flex ">
+        <div>This </div>
+        <div>is </div>
+        <div>Header</div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
