eae99faadc693c80110da926ce056d62861d5e8e
kminsoo0110 <kminsoo0110@naver.com>
2025-10-07 16:31:18 +0900

    Set API in postHeader.jsx

--- src/components/common/postHeader/EmojiDropdown.jsx
+++ src/components/common/postHeader/EmojiDropdown.jsx
@@ -2,8 +2,8 @@
 import { useState, useEffect } from 'react';
 
 const URL = 'https://rolling-api.vercel.app';
-const TEAM = '19-4'; // 우리팀 데이터는 없어서 테스트용도로 임시로 다른 팀꺼로 설정했습니다. 나중에는 URL에 19-3이 추가될 예정.
-const ID = 13850; // 테스트 하려고 임시로 설정한 ID입니다. ID는 받아서 유동적으로 처리될 예정입니다.
+const TEAM = '19-3'; // 우리팀 데이터는 없어서 테스트용도로 임시로 다른 팀꺼로 설정했습니다. 나중에는 URL에 19-3이 추가될 예정.
+const ID = 13971; // 테스트 하려고 임시로 설정한 ID입니다. ID는 받아서 유동적으로 처리될 예정입니다.
 
 function EmojiDropdown() {
   const [emojiList, setEmojiList] = useState([]);
@@ -18,14 +18,10 @@ function EmojiDropdown() {
           },
         });
 
-        if (!res.ok) {
-          throw new Error(`HTTP error! status: ${res.status}`);
-        }
-
         const data = await res.json();
         setEmojiList(data.results);
       } catch (e) {
-        console.error('데이터 불러오기 실패', e);
+        throw new Error(`Api get error: ${e}`);
       }
     };
     fetchEmojis();
