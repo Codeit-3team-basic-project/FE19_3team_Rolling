8438748a7a0b4505e30a063d7396db9238927779
9tnqls6 <9tnqls6@gmail.com>
2025-10-14 01:25:44 +0900

    post생성페이지 라우터 추가, 메인페이지 레이어 정리 및 생성링크버버튼추가

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import List from './pages/List';
 import PostDashBoard from './pages/PostDashBoard';
+import PostMessage from './pages/PostMessage';
 
 export default function AppRouter() {
   return (
@@ -10,6 +11,8 @@ export default function AppRouter() {
         <Route path='/' element={<Home />} />
         <Route path='/list' element={<List />} />
         <Route path='/post' element={<PostDashBoard />} />
+        {/* path에 ID 추가 필요 */}
+        <Route path='/post/message' element={<PostMessage />} />
       </Routes>
     </Router>
   );
