96d6da1009caf7bd2d3ac4976ee58a1c0aff4998
9tnqls6 <9tnqls6@gmail.com>
2025-10-14 00:17:53 +0900

    메인포스트페이지 생성 및 라우터 연결

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import List from './pages/List';
+import PostDashBoard from './pages/PostDashBoard';
 
 export default function AppRouter() {
   return (
@@ -8,6 +9,7 @@ export default function AppRouter() {
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/list' element={<List />} />
+        <Route path='/post' element={<PostDashBoard />} />
       </Routes>
     </Router>
   );
---
+++ src/pages/PostDashBoard.jsx
@@ -0,0 +1,26 @@
+import Header from '../components/common/Header';
+import PostHeader from '../components/common/postHeader/PostHeader';
+import CommentCard from '../components/CommentCard';
+
+function PostDashBoard() {
+  return (
+    <div className='min-h-screen'>
+      <Header />
+      <PostHeader />
+      <div className='w-screen py-110 bg-orange-200'>
+        <div className='w-1200 mx-auto grid grid-cols-3 gap-24 bg-green-400'>
+          <CommentCard comment={'테스트테스트테스트테스트'}></CommentCard>
+          <CommentCard comment={'테스트테스트테스트테스트'}></CommentCard>
+          <CommentCard comment={'테스트테스트테스트테스트'}></CommentCard>
+          <CommentCard comment={'테스트테스트테스트테스트'}></CommentCard>
+          <CommentCard comment={'테스트테스트테스트테스트'}></CommentCard>
+          <CommentCard comment={'테스트테스트테스트테스트'}></CommentCard>
+          <CommentCard comment={'테스트테스트테스트테스트'}></CommentCard>
+          <CommentCard comment={'테스트테스트테스트테스트'}></CommentCard>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PostDashBoard;
