9712707aa3234dd3cc6d844866b4e454d63f184d
kminsoo0110 <84002714+kminsoo0110@users.noreply.github.com>
2025-10-15 06:15:50 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import PostBackground from './pages/postBackground';
 
 export default function App() {
   return (
-    <div className='min-h-screen'>
-      <PostBackground />
+    <div className='min-h-screen bg-gray-100'>
+      <AppRouter />
     </div>
   );
 }
