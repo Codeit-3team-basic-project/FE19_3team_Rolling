18d112070910127cec68c06131602311982af940
9tnqls6 <9tnqls6@gmail.com>
2025-10-14 23:00:45 +0900

    코멘트 카드 삭제 기능 추가

--- src/pages/PostDashBoard.jsx
+++ src/pages/PostDashBoard.jsx
@@ -25,16 +25,21 @@ function PostDashBoard() {
     fetchRecipients();
   }, []);
 
+  // 삭제 함수
+  const handleDelete = id => {
+    setRecipients(prev => prev.filter(recipient => recipient.id !== id));
+  };
+
   return (
     <div className='min-h-screen'>
       <Header />
       <PostHeader />
       {/* 전체 배경 */}
-      <div className='w-screen py-110 bg-orange-200'>
+      <div className='w-screen min-h-[calc(100vh-130px)] py-110 bg-orange-200'>
         {/* 컨텐츠 너비랩 */}
         <div className='w-1200 mx-auto'>
           {/* 버튼 영역 */}
-          <div className='w-full mb-10 bg-blue-400 flex justify-end'>
+          <div className='w-full mb-10 flex justify-end'>
             <Link to='/post/message'>
               {/* 링크 루트에 id값 추가 필요 */}
               <Button variant='primary' size='40' state='enabled' icon=''>
@@ -47,8 +52,8 @@ function PostDashBoard() {
           {loading ? (
             <p>로딩 중...</p>
           ) : (
-            <div className='w-full grid grid-cols-3 gap-24 bg-green-400'>
-              {recipients.map((recipient) => (
+            <div className='w-full grid grid-cols-3 gap-24'>
+              {recipients.map(recipient => (
                 <CommentCard
                   key={recipient.id}
                   avatarSrc={
@@ -63,10 +68,12 @@ function PostDashBoard() {
                   }
                   date={
                     recipient.recentMessages[0]
-                      ? new Date(recipient.recentMessages[0].createdAt).toLocaleDateString()
+                      ? new Date(
+                          recipient.recentMessages[0].createdAt
+                        ).toLocaleDateString()
                       : new Date(recipient.createdAt).toLocaleDateString()
                   }
-                  onDelete={() => console.log('삭제 기능 구현 필요')}
+                  onDelete={() => handleDelete(recipient.id)}
                 />
               ))}
             </div>
